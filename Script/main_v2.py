da4968065f6384211a80111c0bbda04be91b368b
Mariotti-Matteo <matteo.mariotti@istitutoagnelli.it>
2025-05-19 12:51:22 +0200

    Pulsanti

--- Script/main_v2.py
+++ Script/main_v2.py
@@ -277,7 +277,87 @@ class drawn_Platform(pygame.sprite.Sprite):
 
     def update(self):
         screen.blit(self.image, (self.rect.x, self.rect.y))
-        
+
+class Pulsante_Rosso(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill("brown1")
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.premuto = False
+        todraw.add(self)
+    
+    def update(self):
+        if self.premuto == False:
+            screen.blit(self.image, (self.rect.x, self.rect.y))
+        self.premuto = False
+    
+    def pressione(self):
+        self.premuto = True
+
+class Pulsante_Blue(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill("blue4")
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.premuto = False
+        todraw.add(self)
+    
+    def update(self):
+        if self.premuto == False:
+            screen.blit(self.image, (self.rect.x, self.rect.y))
+        self.premuto = False
+    
+    def pressione(self):
+        self.premuto = True
+
+class muro_rosso(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill("brown1")
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.sbloccato = False
+        plats.add(self)
+
+    def update(self):
+        if self.sbloccato == False:
+            screen.blit(self.image, (self.rect.x, self.rect.y))
+        self.sbloccato = False
+
+    def sblocca(self):
+        self.sbloccato = True
+        plats.remove(self)
+
+class muro_blue(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill("blue4")
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.sbloccato = False
+        plats.add(self)
+    def update(self):
+        if self.sbloccato == False:
+            screen.blit(self.image, (self.rect.x, self.rect.y))
+        plats.add(self)
+        self.sbloccato = False
+
+    def sblocca(self):
+        self.sbloccato = True
+        plats.remove(self)
+
+
+
 
 # Le collisioni vengono calcolate separatamente sull'asse x ed y
 def xcoll(player):
@@ -311,6 +391,10 @@ def build(level, move):
     mplats = []
     p1spawnpoint = []
     p2spawnpoint = []
+    red_button = []
+    blue_button = []
+    red_walls = []
+    blue_walls = []
     # displat = []
     myx = 0
     myy = 0
@@ -355,7 +439,19 @@ def build(level, move):
                 # displat.append(p)
             elif c == "D":
                 p = drawn_Platform(myx, myy)
-                # displat.append(p)
+                # displat.append(p)    
+            elif c == "P":
+               p = Pulsante_Rosso(myx, myy)
+               red_button.append(p)
+            elif c =="p":
+               p = Pulsante_Blue(myx, myy)
+               blue_button.append(p)
+            elif c =="r":
+               p = muro_rosso(myx,myy)
+               red_walls.append(p)
+            elif c =="b":
+               p = muro_blue(myx,myy)
+               blue_walls.append(p)
             myx += 50
         myy += 50
         myx = 0
